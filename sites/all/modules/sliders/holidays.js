2abd5a6266d866bceb5f5738456892e8c43cb283
Emile Kott <emilekott@gmail.com>
2012-10-10 11:58:47 +0100

    improved holiday search form

--- sites/all/modules/sliders/holidays.js
+++ sites/all/modules/sliders/holidays.js
@@ -3,17 +3,31 @@
     //add this script to module and only call it on product finder page...
     Drupal.behaviors.node_slideshow = {
         attach: function (context, settings) {
-           var apply = $('input#edit-submit-holiday-lettings');
-           
+            var apply = $('input#edit-submit-holiday-lettings');
+            apply.hide();
           
            
+            $('.form-item-field-features-holiday-tid input[type="checkbox"]').change(function(){
+                $('#edit-update-features-wrapper').show();
+            });
            
+            $('.form-item-field-town-tid input[type="checkbox"]').change(function(){
+                $('#edit-update-town-wrapper').show();
+            });
+           
+            //text fields - budget
+            $('#edit-field-sleeps-value').focus(function(){
+                $('#edit-update-sleeps-wrapper').show(); 
+            });
+            $('#edit-field-sleeps-value-1').focus(function(){
+                $('#edit-update-sleeps-wrapper').show(); 
+            });
            
 
            
-           $('.views-exposed-form select').change(function() {
+            $('.views-exposed-form select').change(function() {
                 apply.click();     
-           });
+            });
            
           
            
